b76a139394ec271f5632f005a0a0e87a2259bd3e
Leandro Martins <leandro.martins.85@gmail.com>
2025-03-07 17:08:56 -0300

    Aula 13 - Manipular dados maliciosos na requisição

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove campos desconhecidos
+      forbidNonWhitelisted: true, // Retorna erro se um campo não permitido for enviado
+      transform: true, // Converte os tipos automaticamente (ex: string para número)
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error(error);
+});
