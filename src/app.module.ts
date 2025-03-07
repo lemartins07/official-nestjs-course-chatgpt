06c0c92ab2bdcab94c6223ad2110c4c724b9c4eb
Leandro Martins <leandro.martins.85@gmail.com>
2025-03-07 17:31:09 -0300

    Módulo 3: Aula 4 - Introdução ao módulo TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TransactionsModule } from './transactions/transactions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest_finance',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    TransactionsModule,
+  ],
 })
 export class AppModule {}
